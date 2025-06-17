0da51c79faf43a8e07f7d202a110a6f948e2ccb0
kubadoPL <kubado515@gmail.com>
2025-06-17 16:39:43 +0200

    Update bot_state.py

--- bot_state.py
+++ bot_state.py
@@ -2,7 +2,7 @@ import os
 import json
 import threading
 import os
-
+import datetime
 # Set working directory to one level up from where bot.py is
 script_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.join(script_dir, "..")
@@ -14,23 +14,23 @@ LOCK = threading.Lock()
 REGISTRY_FILE = os.path.join(os.path.dirname(__file__), "/tmp/bot_registry.json")
 
 def load_registry():
-    with LOCK:
-        if not os.path.exists(REGISTRY_FILE):
-            with open(REGISTRY_FILE, "w") as f:
-                json.dump({}, f)
-        with open(REGISTRY_FILE, "r") as f:
-            return json.load(f)
-
+    if not os.path.exists(REGISTRY_FILE):
+        with open(REGISTRY_FILE, "w") as f:
+            json.dump({}, f)
+    with open(REGISTRY_FILE, "r") as f:
+        return json.load(f)
 
 def save_registry(data):
-    with LOCK:
-        with open(REGISTRY_FILE, "w") as f:
-            json.dump(data, f, indent=2)
+    with open(REGISTRY_FILE, "w") as f:
+        json.dump(data, f)
+    print(f"[DEBUG] Writing registry file to: {REGISTRY_FILE}")
 
-def register_bot(bot_name):
-    registry = load_registry()
-    registry[bot_name] = {"status": "running"}
-    save_registry(registry)
+
+def register_bot(name):
+    data = load_registry()
+    data[name] = {"started": str(datetime.datetime.utcnow())}
+    save_registry(data)
+
 
 def unregister_bot(bot_name):
     registry = load_registry()
@@ -39,5 +39,6 @@ def unregister_bot(bot_name):
         save_registry(registry)
 
 def get_running_bots():
+    print(list(load_registry().keys()))
     return list(load_registry().keys())
 
